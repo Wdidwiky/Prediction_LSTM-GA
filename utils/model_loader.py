895175f69426cf5ed47c7083e8edee1d00c33a82
Wdidwiky <alexferguson.af3@gmail.com>
2026-07-18 21:52:23 +0700

    add debug

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -2,5 +2,10 @@ from keras.models import load_model
 import joblib
 from config import Config
 
-MODEL = load_model(Config.MODEL_PATH)
+print("[DEBUG MODEL PATH]",Config.MODEL_PATH)
+try:
+    MODEL = load_model(Config.MODEL_PATH)
+except:
+    MODEL = load_model("model\lstm_ga_brent.keras")
+
 SCALER = joblib.load(Config.SCALER_PATH)
